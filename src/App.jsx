4bb25a4338ced46264e306215ba3ef6f2f6dc8ea
Antonio Roman <antonioromndelgado@gmail.com>
2024-09-17 12:28:49 +0200

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from "react"
+
+const App = () => {
+
+  return (
+    <>
+      <h1>Hola mundo</h1>
+      <p>Que pasa mi gente</p>
+    </>
+  )
+}
+
+export default App
---
+++ src/components/pages/Inicio.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Incio = () => {
+  return (
+    <>
+
+    </>
+  )
+}
+
+export default Incio
